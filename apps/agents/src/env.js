d702399d3941292c870b2a469f99042e3703c7d5
Ihor <kopach@users.noreply.github.com>
2025-11-18 15:10:59 +0100

    --wip-- [skip ci] zod3

--- apps/agents/src/env.js
+++ apps/agents/src/env.js
@@ -1,13 +1,13 @@
-import { createEnv } from '@t3-oss/env-nextjs';
-import { z } from 'zod';
+const { createEnv } = require('@t3-oss/env-nextjs');
+const { z } = require('zod');
 
-export const env = createEnv({
+const env = createEnv({
   /*
    * Server-only Environment variables
    */
   server: {
-    FOUNDRY_TOKEN: z.jwt({ alg: 'ES256' }),
-    FOUNDRY_TEST_USER: z.uuid(),
+    FOUNDRY_TOKEN: z.string(), // in Zod@4 z.jwt({ alg: 'ES256' }),
+    FOUNDRY_TEST_USER: z.string().uuid(),
     OSDK_CLIENT_SECRET: z.string(),
     OPEN_WEATHER_API_KEY: z.string(),
     LOG_PREFIX: z.string(),
@@ -17,22 +17,22 @@ export const env = createEnv({
     GOOGLE_SEARCH_ENGINE_MARKETS: z.string(),
     GEMINI_API_KEY: z.string(),
     BROWSERFY_KEY: z.string(),
-    BROWSERFY_BROWSER_URL: z.url(),
+    BROWSERFY_BROWSER_URL: z.string().url(),
     RANGR_OSDK_CLIENT_ID: z.string(),
     RANGR_OSDK_CLIENT_SECRET: z.string(),
-    RANGR_FOUNDRY_STACK_URL: z.url(),
+    RANGR_FOUNDRY_STACK_URL: z.string().url(),
     RANGR_ONTOLOGY_RID: z.string(),
-    OFFICE_SERVICE_ACCOUNT: z.email(),
+    OFFICE_SERVICE_ACCOUNT: z.string().email(),
     OPEN_AI_KEY: z.string(),
     SLACK_CLIENT_ID: z.string(),
     SLACK_CLIENT_SECRET: z.string(),
     SLACK_SIGNING_SECRET: z.string(),
     SLACK_BOT_TOKEN: z.string(),
     SLACK_APP_TOKEN: z.string(),
-    SLACK_BASE_URL: z.url(),
+    SLACK_BASE_URL: z.string().url(),
     GSUITE_SERVICE_ACCOUNT: z.string(),
     EIA_API_KEY: z.string(),
-    EIA_BASE_URL: z.url(),
+    EIA_BASE_URL: z.string().url(),
     CA_SERIES_ID: z.string(),
     FIRECRAWL_API_KEY: z.string(),
     GITHUB_PRIVATE_KEY: z.string(),
@@ -42,9 +42,9 @@ export const env = createEnv({
     GITHUB_REPO_OWNER: z.string(),
     GITHUB_REPO_NAME: z.string(),
 
-    FOUNDRY_STACK_URL: z.url(),
+    FOUNDRY_STACK_URL: z.string().url(),
     OSDK_CLIENT_ID: z.string(),
-    REDIRECT_URL: z.url(),
+    REDIRECT_URL: z.string().url(),
     ONTOLOGY_RID: z.string(),
   },
 
@@ -54,9 +54,9 @@ export const env = createEnv({
    * 💡 You'll get type errors if these are not prefixed with NEXT_PUBLIC_.
    */
   client: {
-    NEXT_PUBLIC_FOUNDRY_STACK_URL: z.url(),
+    NEXT_PUBLIC_FOUNDRY_STACK_URL: z.string().url(),
     NEXT_PUBLIC_OSDK_CLIENT_ID: z.string(),
-    NEXT_PUBLIC_REDIRECT_URL: z.url(),
+    NEXT_PUBLIC_REDIRECT_URL: z.string().url(),
     NEXT_PUBLIC_ONTOLOGY_RID: z.string(),
   },
 
@@ -124,3 +124,5 @@ export const env = createEnv({
    */
   emptyStringAsUndefined: true,
 });
+
+module.exports = { env };
